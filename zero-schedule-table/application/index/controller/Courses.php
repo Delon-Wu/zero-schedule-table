90e74d22becedecbd8942639c469bb2a3685d59b
Delon-Wu <2474323762@qq.com>
2019-10-08 13:38:00 +0800

    无课表0.0.2，依旧存在问题

--- zero-schedule-table/application/index/controller/Courses.php
+++ zero-schedule-table/application/index/controller/Courses.php
@@ -15,24 +15,53 @@ class Courses extends Auth{
         $result = $user->where('email',$email)->find();
         if($result){
             $user->where('email',$email)->delete();
+            $user->email = $email;
+            $user->Mon = input('post.Mon') ;                   //Q1：这里在纠结数据到底是在后端处理呢还是前端，另外传数组进数据库不是我的本意:) 是尝试之后的无奈之举 TAT
+            $user->Tue = input('post.Tue') ;
+            $user->Wed = input('post.Wed') ;
+            $user->Thur = input('post.Thur') ;
+            $user->Fri = input('post.Fri') ;
+            $user->Sat = input('post.Sat') ;
+            $user->Sun = input('post.Sun') ;
+            $saveResult = $user->save();
+            if($saveResult){
+                $courseData = $user->select();
+                return json_encode($courseData);
+            }else{
+                return json_encode([
+                    'code' => '0002',
+                    'msg' => 'Something wrong:('
+                ]);
+            }
         }else{
-            $user->email = $email ;
+            $user->email = $email;
             $user->Mon = input('post.Mon') ;
             $user->Tue = input('post.Tue') ;
             $user->Wed = input('post.Wed') ;
             $user->Thur = input('post.Thur') ;
             $user->Fri = input('post.Fri') ;
             $user->Sat = input('post.Sat') ;
-            $user->Sun = input('post.Sun') ;                   //Q3：这里在纠结数据到底是在后端处理呢还是前端，另外传数组进数据库不是我的本意:) 是尝试之后的无奈之举 TAT
-            $result = $user->save();
+            $user->Sun = input('post.Sun') ;
+            $saveResult = $user->save();
+            if($saveResult){
+                $courseData = $user->select();
+                return json_encode($courseData);
+            }else{
+                return json_encode([
+                    'code' => '0002',
+                    'msg' => 'Something wrong:('
+                ]);
+            }
         }
-        if($result){
-            $courseData = $user->select();
-            return json_encode($courseData);
-        }else{
+    }
+    public function load_data(){
+        $user = new CoursesModel();
+        $courseData = $user->select();
+        return json_encode($courseData);
+        if(!$courseData){
             return json_encode([
-                'code' => '0002',
-                'msg' => 'Something wrong:('
+                'code' => '0003',
+                'msg' => 'Fail to query data:('
             ]);
         }
     }
